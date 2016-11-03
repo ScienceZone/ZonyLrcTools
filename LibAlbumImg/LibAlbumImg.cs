482d9dcd4cf3d5eaa4aaf7df6176b618c9bd567f
Zony <mo1995106@163.com>
2016-11-03 21:06:31 +0800

    Add 添加专辑图像下载接口。

---
+++ LibAlbumImg/LibAlbumImg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibPlug;
+using LibPlug.Interface;
+namespace LibAlbumImg
+{
+    [Plugins("专辑图像下载插接件","Zony","从网易云音乐下载专辑图像写入到歌曲文件。",1000,PluginTypesEnum.AlbumImg)]
+    public class LibAlbumImg : IPlug_Lrc
+    {
+        public PluginsAttribute PlugInfo { get; set; }
+
+        public bool DownLoad(string artist, string songName, out byte[] lrcData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
