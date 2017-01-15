2f03fbed96ee9d4e53ceadc719b5bfc9f80c78c2
Zony <mo1995106@163.com>
2017-01-15 22:58:28 +0800

    update 好像没有什么更新b（￣▽￣）d　

---
+++ ZonyLrcTools/Untils/EncodingConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZonyLrcTools.Untils
+{
+    /// <summary>
+    /// 编码转换器
+    /// </summary>
+    public class EncodingConverter
+    {
+        private byte[] _convertedBytes;
+        public EncodingConverter()
+        {
+
+        }
+
+        public byte[] ConvertBytes(byte[] _sourceBytes)
+        {
+            return null;
+        }
+    }
+}
